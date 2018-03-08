47cd10dbac1fe063e905c3f79f5c1186d2e04949
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-03-08 10:41:04 -0800

    updated setInterval'

--- src/App.js
+++ src/App.js
@@ -4,37 +4,83 @@ import './App.css';
 // import CountyMap from './map.js'
 import Chart2 from './barChart.js'
 /* Import Data */
-import * as testData from './test-data.json';
-import * as mapData from './map.json';
+// import * as testData from './test-data.json';
+import * as data from './wild-pig-data.json';
+// import * as mapData from './map.json';
 
 
 class App extends Component {
    constructor(props){
       super(props)
       this.state = {
-         map: {},
-         population: {}
+         year: 2000,
+         uniqueYears: [],
+         data: [],
+         paused: false
       }
    };
 
 
-  componentWillMount(){
-      // used to asynchronously load topojson maps and data{
-      this.setState({
-         map: mapData,
-         population: testData["PIG POPULATIONS"]
+   filterData(year) {
+
+      var results = []
+      data["PIG POPULATIONS"].forEach(function(item){
+        if(item.year === year){
+          results.push(item)
+        }
       })
-      
-      
-    }  
+
+      this.setState({
+      data: results
+      })  
+   }
+
+   changeData() {
+
+      var rawData = data["PIG POPULATIONS"]
+      var uniqueYears = [];
+  
+      rawData.forEach(function(item){
+         if(!uniqueYears.includes(item.year)){
+            uniqueYears.push(item.year)
+         }
+      })
+
+      this.setState({uniqueYears: uniqueYears})
+   }
+
+
+   componentDidMount () {
+
+      //this.filterData(this.state.year) 
+      this.changeData()
+      var yearsArray = this.state.uniqueYears
+
+      var i = -1;
+      (function f(){
+         i = (i+1) % yearsArray.length;
+         this.filterData(yearsArray[i])
+         setTimeout(f, 5000) 
+      })
+
+      // Run the Set time out in here 
+      //this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+
+   }
+
+      // componentWillUnmount() {
+   //   clearInterval(this.interval);
+   // }
+  
  
 
 
   render() {
+   console.log(this.state)
     return (
 
       <div className="App">
-      <Chart2 data = {this.state.population}/>
+      <Chart2 data = {this.state.data}/>
       </div>
     );
   }
