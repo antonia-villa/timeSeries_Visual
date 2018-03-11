ce09dff722135ec513d6c8ea08dfe2a72dc6d939
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-03-11 12:01:58 -0700

    updated tooltip content

--- src/barChart.js
+++ src/barChart.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 
 
-import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, LabelList} from 'recharts';
+import {ResponsiveContainer, BarChart, Bar, XAxis, YAxis, Tooltip, LabelList} from 'recharts';
 import {reFormat} from './dataCleansing.js'
 
 class Chart extends Component {
   render() {
     // Reformat input data
     const data = reFormat(this.props.data);
+    // Customize Data Labels
     const CustomizedLabel = (props) => {
     const {x, y, value, width, height} = props;
-    console.log(width)
       return (
         <text 
-           x={x + width / 2}
+           x={(x-5) + width / 2}
            y={y} 
-           dy={-4} 
+           dy={-5} 
            fontSize='14' 
            fontFamily='Merriweather Sans'
            fill="black"
@@ -23,8 +23,23 @@ class Chart extends Component {
            >
            {value}%
         </text>
-        );
-      }
+      );
+    }
+
+    // Customize Tooltip Content
+    const renderTooltip = (props) => {
+      const { active, payload } = props;
+        if (active && payload && payload.length) {
+          const data = payload[0].payload;
+          return (
+            <div className="customizedToolTip">
+              <p className="ToolTipTitle">{data.island}</p>
+              <p className="ToolTipText">Total: {data.percent}%</p>
+              <p className="ToolTipText">Population: {data.pigPopulation.toLocaleString()}</p>
+            </div>
+          );
+        }
+    }
 
 
   return(
@@ -35,10 +50,13 @@ class Chart extends Component {
          <XAxis 
              dataKey="island"
              fontFamily="Merriweather Sans"
-             tick={{ fill: 'black' }}
-             fontWeight="bold" 
+             tick={{ fill: 'black', dy: 5 }}
+             fontWeight="normal" 
+             
              />
          <YAxis hide/>
+         <Tooltip  content={renderTooltip}
+         cursor={false}/>
          <Bar 
              dataKey="percent" 
              barSize ={200}
